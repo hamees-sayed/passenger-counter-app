ae2fb5053c21727988c721bed1d891384c2a2c49
hamees-sayed <hameessayed71@gmail.com>
2022-10-07 22:30:35 +0530

    added the complete app

---
+++ index.js
@@ -0,0 +1,21 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+
+console.log(countEl)
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function decrement() {
+    count -= 1
+    countEl.innerText = count
+}
+
+function save() {
+    let entriesString = count + ", "
+    saveEl.innerText += " " + entriesString
+}
